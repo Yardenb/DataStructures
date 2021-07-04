9d6bb7925426c49a673e2148a83bf7c952761b19
Yardenb <37038678+Yardenb@users.noreply.github.com>
2021-07-04 09:33:28 +0300

    Added size property. Changed the returned values for some of the methods

--- linkedLists/basicLinkedList.js
+++ linkedLists/basicLinkedList.js
@@ -9,8 +9,9 @@ class SingleLinkedList {
     constructor(){
         this.head = null;
         this.tail = null;
+        this.size = 0;
     }
-
+    //Returns list
     prepend(node){
         //If it's the first element
         if(!this.tail){
@@ -21,9 +22,10 @@ class SingleLinkedList {
         //Update head pointer
         this.head = node;
 
+        this.size++;
         return this;
     }
-
+    //Returns list
     append(node){
         //If it's the first element
         if(!this.head){
@@ -35,44 +37,61 @@ class SingleLinkedList {
         //Update tail to point at the new node
         this.tail = node;
 
+        this.size++;
         return this;
     }
+    //Returns the deleted node
     deleteHead(){
         //0 elements in list
         if(!this.head){
             return this;
         }
+        let deletedNode = this.head;
         this.head = this.head.next;
-        return this;
+        this.size--;
+        return deletedNode;
     }
+    //Returns the deleted node
     deleteTail(){
         //If there are no elements
         if(!this.tail){
             return this;
         }
+
+        let deletedNode;
+
         //If there is 1 element
         if(!this.head.next){
+            deletedNode = this.head;
             this.head = this.tail = null
+            this.size--;
+            return deletedNode;
         }
         let currentNode = this.head;
         let nextNode = currentNode.next;
+        //Go until we have tail in our nextNode
         while(nextNode.next){
             currentNode = nextNode;
             nextNode = nextNode.next;
         }
+        deletedNode = nextNode;
         this.tail = currentNode;
         this.tail.next = null;
-        return this;
+        this.size--;
+        return deletedNode;
     }
+    _isEqual(data1, data2){
+        return data1 === data2;
+    }
+
     //TODO - implement the following:
-    //1. Delete head - DONE
-    //2. Delete tail - DONE
-    //3. Delete by value 
-    //4. Find by value
-    //5. Reverse
-    //6. Create array from list
-    //7. Create list from array
-    //8. Delete all
+    //1. Delete by value 
+    //2. Find by value
+    //3. Reverse
+    //4. Create array from list
+    //5. Create list from arrays
+    //6. Delete all
+    //7. Add size property
 }
 
 const list = new SingleLinkedList();
