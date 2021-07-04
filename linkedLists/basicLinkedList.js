0981c33a0c2744fd75933a884ee99189bbefd0a4
Yardenb <37038678+Yardenb@users.noreply.github.com>
2021-07-05 00:29:10 +0300

    Added deleteByValue, findByValue, createArray, createFromArray, deleteAll methods. Also added size property

--- linkedLists/basicLinkedList.js
+++ linkedLists/basicLinkedList.js
@@ -6,10 +6,13 @@ class LinkListNode {
 }
 
 class SingleLinkedList {
-    constructor(){
+    constructor(arr){
         this.head = null;
         this.tail = null;
         this.size = 0;
+        if(arr){
+            this.createFromArray(arr);
+        }
     }
     //Returns list
     prepend(node){
@@ -31,7 +34,6 @@ class SingleLinkedList {
         if(!this.head){
             this.head = node;
         } else {
-            //Link the new node
             this.tail.next = node;
         }
         //Update tail to point at the new node
@@ -42,11 +44,10 @@ class SingleLinkedList {
     }
     //Returns the deleted node
     deleteHead(){
-        //0 elements in list
         if(!this.head){
             return this;
         }
-        let deletedNode = this.head;
+        const deletedNode = this.head;
         this.head = this.head.next;
         this.size--;
         return deletedNode;
@@ -83,15 +84,64 @@ class SingleLinkedList {
     _isEqual(data1, data2){
         return data1 === data2;
     }
-
+    //Returns the desired node/null
+    findByValue(value){
+        let currentnode = this.head;
+        while(currentnode){
+            if(this._isEqual(value, currentnode.data)){
+                return currentnode;
+            }
+            currentnode = currentnode.next;
+        }
+        return null;
+    }
+    deleteAll(){
+        while(this.head){
+            this.deleteHead();
+        }
+    }
+    deleteByValue(valueToDelete){
+        if(!this.head){
+            return;
+        }
+        let currentNode = this.head;
+        if(this._isEqual(valueToDelete, currentNode.data)){
+            return this.deleteHead();
+        }
+        
+        let nextNode = currentNode.next;
+        while(nextNode){
+            if(this._isEqual(valueToDelete, nextNode.data)){
+                currentNode.next = nextNode.next;
+                this.size--;
+                //If the deleted node was the tail - reassign the tail
+                if(!currentNode.next){
+                    this.tail = currentNode;
+                }
+                return nextNode;
+            }
+            currentNode = nextNode;
+            nextNode = currentNode.next;
+        }
+    }
+    createArray(){
+        let currentNode = this.head;
+        const arr = [];
+        while(currentNode){
+            arr.push(currentNode.data);
+            currentNode = currentNode.next;
+        }
+        return arr;
+    }
+    //Should be private
+    createFromArray(arr){
+        for(let i = 0; i < arr.length ; i++){
+            this.append(new LinkListNode(arr[i]))
+        }
+    }
     //TODO - implement the following:
-    //1. Delete by value 
-    //2. Find by value
-    //3. Reverse
-    //4. Create array from list
-    //5. Create list from arrays
-    //6. Delete all
-    //7. Add size property
+    //1. Reverse
+
 }
 
 const list = new SingleLinkedList();
@@ -101,3 +151,4 @@ const node3 = new LinkListNode('test 3');
 list.append(node);
 list.append(node2);
 list.append(node3);
+console.log(list);
